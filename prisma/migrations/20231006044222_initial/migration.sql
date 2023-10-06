4a0424d5ce367ff82ebaa5033953aa6b0dc61362
pabel1 <pabelahamed39@gmail.com>
2023-10-06 10:43:59 +0600

    student academic faculty semester department all model created with relationship

---
+++ prisma/migrations/20231006044222_initial/migration.sql
@@ -0,0 +1,94 @@
+-- CreateTable
+CREATE TABLE "Academic_Semester" (
+    "id" TEXT NOT NULL,
+    "year" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "startMonth" TEXT NOT NULL,
+    "endMonth" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Academic_Semester_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Academic_Faculty" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Academic_Faculty_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Academic_Department" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "academicFacultyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Academic_Department_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "students" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "middleName" TEXT NOT NULL,
+    "profileImage" TEXT,
+    "email" TEXT NOT NULL,
+    "contactNo" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "bloodGroup" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "academicsemesterId" TEXT NOT NULL,
+    "academicDepartmentId" TEXT NOT NULL,
+    "academicFacultyId" TEXT NOT NULL,
+
+    CONSTRAINT "students_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "faculties" (
+    "id" TEXT NOT NULL,
+    "facultyId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "middleName" TEXT NOT NULL,
+    "profileImage" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "contactNo" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "bloodGroup" TEXT NOT NULL,
+    "designation" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "academicDepartmentId" TEXT NOT NULL,
+    "academicFacultyId" TEXT NOT NULL,
+
+    CONSTRAINT "faculties_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Academic_Department" ADD CONSTRAINT "Academic_Department_academicFacultyId_fkey" FOREIGN KEY ("academicFacultyId") REFERENCES "Academic_Faculty"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "students" ADD CONSTRAINT "students_academicsemesterId_fkey" FOREIGN KEY ("academicsemesterId") REFERENCES "Academic_Semester"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "students" ADD CONSTRAINT "students_academicDepartmentId_fkey" FOREIGN KEY ("academicDepartmentId") REFERENCES "Academic_Department"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "students" ADD CONSTRAINT "students_academicFacultyId_fkey" FOREIGN KEY ("academicFacultyId") REFERENCES "Academic_Faculty"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "faculties" ADD CONSTRAINT "faculties_academicDepartmentId_fkey" FOREIGN KEY ("academicDepartmentId") REFERENCES "Academic_Department"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "faculties" ADD CONSTRAINT "faculties_academicFacultyId_fkey" FOREIGN KEY ("academicFacultyId") REFERENCES "Academic_Faculty"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
